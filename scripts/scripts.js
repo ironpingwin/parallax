9519aac99721812d32c12c32e2ea231d74452b27
Maciej Skrzypkowski <m.skrzypkowski@o2.pl>
2013-09-30 23:05:42 +0200

    slider w laptopie

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,15 @@
 
 
-$(function(){
-	$.stellar({
-		horizontalScrolling: false,
-		verticalOffset: 40
-	});
-});
+var CENTRAL_SQUARE_SHIFT = 104;
+var LAPTOP_HALF_WIDTH = 440;
+var SLIDER_CENTER_DIST = 600;
+
+// $(function(){
+// 	$.stellar({
+// 		horizontalScrolling: false,
+// 		verticalOffset: 40
+// 	});
+// });
 
 function scrollToClick(e) {
 	var hash = this.hash;
@@ -17,4 +21,24 @@ $( document ).ready(function() {
 	$('#aFirst').click(scrollToClick);
 	$('#aSecond').click(scrollToClick);
 	$('#aThird').click(scrollToClick);
+	
+	var center = $(document).width() / 2;
+	var left = ($(document).width() - $('#centralSquare').width()) / 2 + CENTRAL_SQUARE_SHIFT;
+	$('#centralSquare').css('left', left + 'px');
+	
+	$('#laptopSlider').slidesjs({
+		width: 880,
+		height: 550
+	});
+	
+	$('#laptopSliderLeft').click(function() {
+		$('#laptopSliderLeftDummy').click();
+		return false;
+    });
+	
+	$('#laptopSliderRight').click(function() {
+		$('#laptopSliderRightDummy').click();
+		return false;
+    });
+
 });
