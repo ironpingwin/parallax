0dff1768fa67b0ac5d85b9e58eb0fb5da3eeb46b
Maciej Skrzypkowski <m.skrzypkowski@o2.pl>
2013-10-09 22:26:25 +0200

    synchronizacja stronicowania slidera z przełączaniem pojedynczym

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,22 +31,108 @@ $( document ).ready(function() {
 		height: 468
 	});
 	
-	$('#laptopSliderLeft').click(function() {
-		$('#laptopSliderLeftDummy').click();
-		return false;
-    });
-	
-	$('#laptopSliderRight').click(function() {
-		$('#laptopSliderRightDummy').click();
-		return false;
-    });
 
 });
 
-// click on apriopriate page in slider given as div
-function pageClick(div, number, clicked) {
-	$('#' + div + ' .slidesjs-pagination a')[number].click();
-	$(clicked).siblings().attr( "class", "paginationRect unselectedRectDark");
-	$(clicked).attr( "class", "paginationRect selectedRect");
+
+
+
+function Slider(id, pictStruct) {
+	this.id_ = id;
+	this.pictStruct_ = pictStruct;
+	this.activePicture_ = 0;	
+
+	this.pictures_ = 0;
+	for (var i in this.pictStruct_) {
+		this.pictures_ += this.pictStruct_[i];
+	}
 }
+
+Slider.prototype.prevPict = function() {
+	$('#' + this.id_ + 'SliderLeft' + 'Dummy').click();
+	this.activePicture_--;
+	if (this.activePicture_ < 0)
+		this.activePicture_ = this.pictures_ -1;
+	this.pageClickByNo();
+	return false;
+}
+
+Slider.prototype.nextPict = function() {
+	$('#' + this.id_ + 'SliderRight' + 'Dummy').click();
+	this.activePicture_++;
+	if (this.activePicture_ == this.pictures_)
+		this.activePicture_ = 0;
+	this.pageClickByNo();
+	return false;
+}
+
+/* Counts page number per given picture number.
+* One page contains few pictures.
+*/
+Slider.prototype.findPage = function(number) {
+	var p = 0;
+	var pictNo = 0;
+	for (var i in this.pictStruct_) {
+		pictNo += this.pictStruct_[i];
+		if (number < pictNo)
+			break;
+		p += 1;
+	}
+
+	return p;
+}
+
+/* Counts number of picture which represent given page */
+Slider.prototype.findPict = function(pageNo) {
+	var pictNo = 0;
+	for (var i=0; i<pageNo; ++i) {
+		pictNo += this.pictStruct_[i];
+	}
+	return pictNo;
+}
+
+Slider.prototype.highlightPage = function(page) {
+	$(page).siblings().attr( "class", "paginationRect unselectedRectDark");
+	$(page).attr( "class", "paginationRect selectedRect");
+}
+
+Slider.prototype.pageClickByNo = function() {
+	var pageNo = this.findPage(this.activePicture_);
+	var child = $('#' + this.id_ + "Text .pagination").children()[pageNo];
+	this.highlightPage(child);	
+}
+
+// click on apriopriate page in slider given as div
+Slider.prototype.pageClick = function(clicked) {
+	var childNo = 0;
+	for (c in $(clicked).parent().children()) {		// find number of clicked page
+		if ($(clicked).parent().children()[c] == clicked) {
+			break;
+		}
+		++childNo;
+	}
+	
+	var pictNo = this.findPict(childNo);
+	this.activePicture_ = pictNo;
+	$('#' + this.id_ + 'Slider' + ' .slidesjs-pagination a')[pictNo].click();
+	this.highlightPage(clicked);
+}
+
+
+//TODO trzeba klasę zrobić na te slidery bo będzie ich kilka
+// Klasa już jest tylko trzeba ją dobrze uzupełnić o IDki i inne wspólne rzeczy
+// już działa tylko jak się klika szybciej niż przewija, to jest błąd...
+// prznieść slidera do oddzielnego pliku
+
+
+/* Number of pictures per single page */
+var laptopPictPerPage = {
+	0: 2,
+	1: 3,
+	2: 2,
+	3: 1,
+};
+
+var laptopSlider = new Slider('laptop', laptopPictPerPage);
+
 
